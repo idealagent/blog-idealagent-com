327628ad716bab2d71cceec3e38040c481a7e43c
Zavier Rodriguez <zavierrodriguez@users.noreply.github.com>
2025-08-27 09:54:36 -0400

    Fix image in search

--- src/layouts/SearchBar.vue
+++ src/layouts/SearchBar.vue
@@ -1,55 +1,55 @@
 <script setup lang="ts">
-import { ref, onMounted, watch } from "vue";
-import config from "@/config/config.json";
-import dateFormat from "@/lib/utils/dateFormat.ts";
-import { humanize, slugify } from "@/lib/utils/textConverter";
-import Fuse from "fuse.js";
-import { IconCalendar1, IconTags } from "@iconify-prerendered/vue-lucide";
+import { ref, onMounted, watch } from "vue"
+import config from "@/config/config.json"
+import dateFormat from "@/lib/utils/dateFormat.ts"
+import { humanize, slugify } from "@/lib/utils/textConverter"
+import Fuse from "fuse.js"
+import { IconCalendar1, IconTags } from "@iconify-prerendered/vue-lucide"
 
-const { summary_length } = config.settings;
+const { summary_length } = config.settings
 
 interface SearchItem {
-  slug: string;
-  data: any;
-  content: any;
+  slug: string
+  data: any
+  content: any
 }
 
 interface Props {
-  searchList: SearchItem[];
+  searchList: SearchItem[]
 }
 
-const props = defineProps<Props>();
-const inputRef = ref<HTMLInputElement | null>(null);
-const inputVal = ref("");
-const searchResults = ref<any[]>([]);
+const props = defineProps<Props>()
+const inputRef = ref<HTMLInputElement | null>(null)
+const inputVal = ref("")
+const searchResults = ref<any[]>([])
 
 const fuse = new Fuse(props.searchList, {
   keys: ["data.title", "data.categories", "data.tags"],
   includeMatches: true,
   minMatchCharLength: 2,
   threshold: 0.5,
-});
+})
 
 onMounted(() => {
-  const searchUrl = new URLSearchParams(window.location.search);
-  const searchStr = searchUrl.get("q");
-  if (searchStr) inputVal.value = searchStr;
-});
+  const searchUrl = new URLSearchParams(window.location.search)
+  const searchStr = searchUrl.get("q")
+  if (searchStr) inputVal.value = searchStr
+})
 
 watch(inputVal, (newVal) => {
-  const results = newVal.length > 2 ? fuse.search(newVal) : [];
-  searchResults.value = results;
-
+  const results = newVal.length > 2 ? fuse.search(newVal) : []
+  searchResults.value = results
+  console.log(results)
   if (newVal.length > 0) {
-    const searchParams = new URLSearchParams(window.location.search);
-    searchParams.set("q", newVal);
+    const searchParams = new URLSearchParams(window.location.search)
+    searchParams.set("q", newVal)
     const newRelativePathQuery =
-      window.location.pathname + "?" + searchParams.toString();
-    history.pushState(null, "", newRelativePathQuery);
+      window.location.pathname + "?" + searchParams.toString()
+    history.pushState(null, "", newRelativePathQuery)
   } else {
-    history.pushState(null, "", window.location.pathname);
+    history.pushState(null, "", window.location.pathname)
   }
-});
+})
 </script>
 
 <template>
@@ -84,7 +84,7 @@ watch(inputVal, (newVal) => {
         >
           <img
             class="group-hover:scale-[1.03] transition duration-300 w-full"
-            :src="item.data.image"
+            :src="item.data.image.src"
             :alt="item.data.title"
             width="445"
             height="230"
@@ -93,7 +93,7 @@ watch(inputVal, (newVal) => {
 
         <ul class="mt-6 mb-4 flex flex-wrap items-center text-text">
           <li class="mr-5 flex items-center flex-wrap font-medium">
-<!--            <Icon name="tabler:calendar" class="mr-1 h-5 w-5 text-gray-600" />-->
+            <!--            <Icon name="tabler:calendar" class="mr-1 h-5 w-5 text-gray-600" />-->
             <IconCalendar1 class="mr-1 h-5 w-5 text-gray-500" />
             <span>{{ dateFormat(item.data.date) }}</span>
           </li>
